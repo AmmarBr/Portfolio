5f0eb978ac9d41028f48110f856bee5d7804de5b
AmmarBr <ammarbrakat731@gmail.com>
2025-06-10 03:15:09 +0300

    test2 commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,56 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import react from 'eslint-plugin-react'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react'; // <-- 1. Import the plugin
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
-  { ignores: ['dist'] },
+  // Global ignores
+  { ignores: ['dist/'] },
+
+  // ESLint recommended base rules
+  js.configs.recommended,
+
+  // React core rules
   {
     files: ['**/*.{js,jsx}'],
+    plugins: {
+      react: react, // <-- 2. Register the plugin
+    },
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
       },
     },
+    rules: {
+      ...react.configs.recommended.rules, // <-- 3. Add recommended React rules
+      'react/react-in-jsx-scope': 'off', // Not needed for React 17+
+      'react/jsx-uses-react': 'off',     // Not needed for React 17+
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect React version
+      },
+    },
+  },
+
+  // React Hooks and Refresh rules
+  {
+    files: ['**/*.{js,jsx}'],
     plugins: {
-      'react': react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-uses-react': 'off', // Disable react jsx pragma rule (React 17+)
-      'react/react-in-jsx-scope': 'off', // Not needed with React 17 JSX Transform
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'prettier', // Disable conflicting Prettier rules
-    ],
   },
-]
+];
